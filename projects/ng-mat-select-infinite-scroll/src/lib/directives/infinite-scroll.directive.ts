737e457eb800fd6114027b1b1c962b71f0507fcf
Haidar Zeineddine <HaidarZ1994@gmail.com>
2019-02-17 02:49:42 +0200

    Add threshold input to allow preload before reaching bottom.

--- projects/ng-mat-select-infinite-scroll/src/lib/directives/infinite-scroll.directive.ts
+++ projects/ng-mat-select-infinite-scroll/src/lib/directives/infinite-scroll.directive.ts
@@ -1,36 +1,60 @@
-import {AfterViewInit, Directive, ElementRef, EventEmitter, Input, OnDestroy, Output} from '@angular/core';
+import {AfterViewInit, Directive, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
 import {MatSelect, SELECT_ITEM_HEIGHT_EM, SELECT_PANEL_MAX_HEIGHT} from '@angular/material';
 import {auditTime, takeUntil, tap} from 'rxjs/operators';
-import {fromEvent, Subject} from 'rxjs';
+import {fromEvent, Subject, Subscription} from 'rxjs';
 
 @Directive({
   selector: '[msInfiniteScroll]'
 })
-export class InfiniteScrollDirective implements OnDestroy, AfterViewInit {
+export class InfiniteScrollDirective implements OnInit, OnDestroy, AfterViewInit {
 
+  @Input() threshold = '15%';
   @Input() complete: boolean;
   @Output() infiniteScroll = new EventEmitter<null>();
+
+  private thrPx = 0;
+  private thrPc = 0;
+
   private onDestroy = new Subject<void>();
+  private scrollSubscription: Subscription;
 
   constructor(private element: ElementRef, private matSelect: MatSelect) {
+  }
+
+  ngOnInit() {
+    this.evaluateThreshold();
   }
 
   ngAfterViewInit() {
     this.matSelect.openedChange.subscribe((opened) => {
       if (opened) {
-        this.registerOriginInfiniteScroll();
+        this.registerScrollListener();
       }
     });
   }
 
   ngOnDestroy() {
+    if (this.scrollSubscription && this.scrollSubscription.unsubscribe) {
+      this.scrollSubscription.unsubscribe();
+    }
     this.onDestroy.next();
     this.onDestroy.complete();
   }
 
-  registerOriginInfiniteScroll() {
+  evaluateThreshold() {
+    if (this.threshold.lastIndexOf('%') > -1) {
+      this.thrPx = 0;
+      this.thrPc = (parseFloat(this.threshold) / 100);
+
+    } else {
+      this.thrPx = parseFloat(this.threshold);
+      this.thrPc = 0;
+    }
+  }
+
+  registerScrollListener() {
     const panel = this.matSelect.panel.nativeElement;
-    fromEvent(panel, 'scroll').pipe(
+    this.scrollSubscription = fromEvent(panel, 'scroll').pipe(
       takeUntil(this.onDestroy),
       auditTime(300),
       tap((event) => {
@@ -40,11 +64,14 @@ export class InfiniteScrollDirective implements OnDestroy, AfterViewInit {
   }
 
   loadAllOnScroll(event) {
-    const singleItemSize = this.getItemHeightPx();
-    const countOfRenderedItems = this.matSelect.options.length;
-    const infiniteScrollDistance = singleItemSize * countOfRenderedItems;
-    const scrolledUntilNow = SELECT_PANEL_MAX_HEIGHT + event.target.scrollTop;
-    if (scrolledUntilNow >= infiniteScrollDistance && !this.complete) {
+    const singleOptionHeight = this.getItemHeightPx();
+    const countOfRenderedOptions = this.matSelect.options.length;
+    const infiniteScrollDistance = singleOptionHeight * countOfRenderedOptions;
+    const threshold = this.thrPc !== 0 ? (infiniteScrollDistance * this.thrPc) : this.thrPx;
+
+    const scrolledDistance = SELECT_PANEL_MAX_HEIGHT + event.target.scrollTop;
+
+    if ((scrolledDistance + threshold) >= infiniteScrollDistance && !this.complete) {
       this.infiniteScroll.emit();
     }
   }
