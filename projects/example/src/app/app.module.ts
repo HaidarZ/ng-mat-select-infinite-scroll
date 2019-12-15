b60e6837251fd56e562964ee53278f57ad1b26c9
Haidar Zeineddine <haidar.zeineddine@gmail.com>
2019-12-16 00:12:17 +0200

    fix(example-app): properly import form-field from the correct module

--- projects/example/src/app/app.module.ts
+++ projects/example/src/app/app.module.ts
@@ -3,7 +3,7 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {MatSelectInfiniteScrollModule} from 'ng-mat-select-infinite-scroll';
-import {MatFormFieldModule} from '@angular/material/form';
+import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FormsModule} from '@angular/forms';
