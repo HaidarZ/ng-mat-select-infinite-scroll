910729198ec40d5ab6d9f1ca7b1f398eba9f7fe4
Haidar Zeineddine <hzeineddine@arabiagis.com>
2019-12-16 10:57:12 +0200

    fix(lib-module): import mat-select module properly

--- projects/ng-mat-select-infinite-scroll/src/lib/mat-select-infinite-scroll.module.ts
+++ projects/ng-mat-select-infinite-scroll/src/lib/mat-select-infinite-scroll.module.ts
@@ -1,9 +1,12 @@
 import {NgModule} from '@angular/core';
 import {MatSelectInfiniteScrollDirective} from './mat-select-infinite-scroll.directive';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [MatSelectInfiniteScrollDirective],
-  imports: [],
+  imports: [
+    MatSelectModule
+  ],
   exports: [MatSelectInfiniteScrollDirective]
 })
 export class MatSelectInfiniteScrollModule {
