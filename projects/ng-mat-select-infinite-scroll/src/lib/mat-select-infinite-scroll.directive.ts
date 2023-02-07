f3e8e6ad4888c330073a88f096641d7a30e03ca2
naciriii <nnacir1938@hotmail.com>
2023-02-07 20:48:43 +0100

    chore: Update to angular 15

--- projects/ng-mat-select-infinite-scroll/src/lib/mat-select-infinite-scroll.directive.ts
+++ projects/ng-mat-select-infinite-scroll/src/lib/mat-select-infinite-scroll.directive.ts
@@ -1,5 +1,5 @@
 import {AfterViewInit, Directive, EventEmitter, Input, NgZone, OnDestroy, OnInit, Output} from '@angular/core';
-import {MatSelect} from '@angular/material/select';
+import {MatLegacySelect as MatSelect} from '@angular/material/legacy-select';
 import {debounceTime, takeUntil, tap} from 'rxjs/operators';
 import {fromEvent, Subject} from 'rxjs';
 
